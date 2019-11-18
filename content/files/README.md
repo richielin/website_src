dcf4a1df49b3682d153166b14ca1deab5b234775
Richie Lin <richielin@users.noreply.github.com>
2019-11-18 00:47:01 -0500

    Update website Nov 2019

--- content/files/README.md
+++ content/files/README.md
@@ -2,5 +2,4 @@
 
 Thanks for visiting  
 [Link](http://taolinrichie.com)  
-Special thanks to Ken Lai for this awesome theme.  
-And also thanks to [Evan Misshula](http://misshula.org) and Adam Fera for the inspiration and help in the development.
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 
 #### TITLE AND AUTHOR SETTINGS ####
 AUTHOR = u'Richie'
-SITENAME = u'Tao Lin'
-SITESUBTITLE = 'AKA. Richie - Data Science, Criminal Justice, Business and More.'
+SITENAME = u'Richie Lin'
+#SITESUBTITLE = 'aka. Richie'
+SITESUBHEADER = 'Time\'s Limited, Gotta Keep Moving.'
 SITEURL = 'http://taolinrichie.com'
 
 #### INPUT AND OUTPUT SETTING ####
@@ -28,7 +29,7 @@ AUTHOR_FEED_RSS = None
 
 
 #### THEME SETTINGS ####
-THEME = '/home/richie/projects/pelican_themes/BT3-Flat'
+THEME = '../BT3-Flat'
 
 # Other pics
 FAVICON = "/images/icon-r.jpg"
@@ -39,7 +40,7 @@ BACKGROUND_IMAGE = '/images/bg-pic.jpg'
 
 # About me
 PERSONAL_PHOTO = "/images/avatar-274x274.png"
-PERSONAL_INFO = '<p align="left">Hi! This is Tao Lin and people call me Richie.</p> <p align="left">I am a data scientist currently working as a research analyst for The Misdemeanor Justice Project at John Jay College of Criminal Justice, City University of New York.</p> <p align="left">I am also a prior Professional Marketing Manager at Pepsi Beverage West North-China Region.</p> <p align="left">I graduated from Texas A&M International University with a Masters degree in Management Information System.</p> <p align="left">The title picture was taken on my first visit to New York City, the beautiful "concrete jungle where dreams are made of".</p>'
+PERSONAL_INFO = '<p align="left">Hi! I am Tao Lin and I like people to call me Richie.</p> <p align="left">I am a data scientist currently working as a research associate for The Data Collaborative for Justice at John Jay College of Criminal Justice, the City University of New York. I am also a technical consultant at the New York City Mayor\'s Office of Criminal Justice.</p> <p align="left">Before I started the journey in the Criminal Justice field, I was a Marketing Manager at Pepsi Beverage of West North-China West Region.</p>'
 
 # LOADING SETTING
 LOAD_CONTENT_CACHE = False
@@ -53,24 +54,14 @@ POST_LIMIT = 1
 DISPLAY_PAGES_ON_MENU = True
 
 #### Working samples ####
-WORK_DESCRIPTION = "Here are some projects I have done."
+WORK_DESCRIPTION = " "
 WORK_LIST = (('link','files/2003-2006 NYC SQF mapping/SQF_icon.png','NYPD 2003 - 2006 Stop, Question and Frisk Mapping',
 'An analysis at MJP','files/2003-2006 NYC SQF mapping/03-06NYC_SQF_maping.html'),
 ('link','/files/GermanCreditAnalysis/index.png','Prediction methods analysis with the German Credit Data set','A presentation at RangTech','https://rpubs.com/zmqplintao/139641'),
 ('link','/files/NBA_conference paper/img_NBA_conf_paper.png','Predicting Sports Outcomes with a Rank-And-Choose Variable Selection Process',"An Analysis published at ABDA.15 conference during my Master's",'http://worldcomp-proceedings.com/proc/p2015/ABD6305.pdf'),
 )
 
-
-
-
-
 #INDEX_CN = True
-
-
-
-
-
-
 
 
 #### THEME SETTINGS ENDS ####
@@ -78,15 +69,12 @@ WORK_LIST = (('link','files/2003-2006 NYC SQF mapping/SQF_icon.png','NYPD 2003 -
 
 
 # Blogroll
-LINKS = (('The Misdemeanor Justice Project','http://misdemeanorjustice.org'),
-	('Evan Misshula', 'https://Misshula.org'),
-	('R Server', 'http://server.taolinrichie.com:8787'),
-        ('Py Server', 'https://server.taolinrichie.com:8888'),
+LINKS = (('Data Collaborative for Justice','https://datacollaborativeforjustice.org/'),
+			('NYC Mayor\'s Office of Criminal Justice','https://criminaljustice.cityofnewyork.us/')
 )
 
 # Social widget
 SOCIAL = (('linkedin','https://www.linkedin.com/in/taolinrichie/'),
-          ('twitter','https://twitter.com/richiefreaky'),
 	  ('github', 'https://github.com/richielin'),
 	  ('envelope', 'mailto:richielin321@gmail.com'),
 
@@ -104,4 +92,4 @@ DEFAULT_PAGINATION = True
 
 
 # Copy right
-COPYRIGHT = '2017 &copy; All Rights Reserved. Thanks to my family, friends and the awesome MJP team for the support.Thanks to <a href="https://misshula.org">Evan Misshula</a> and Adam Fera for the inspiration and guide in the development. This website is developed under Python enpowered with Pelican, also Markdown, HTML and CSS. Thanks to <a href="https://github.com.KenMercusLai/BT3-Flat">Ken Lai</a> for the theme. '
+COPYRIGHT = '2019 &copy; All Rights Reserved.'
